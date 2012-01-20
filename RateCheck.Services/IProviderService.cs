65b7ba5a1ae1708a5dd92132364b31088954d42d
Aman Thakral <a@thakral.in>
2012-01-20 18:32:47 -0500

    Test, services project added

---
+++ RateCheck.Services/IProviderService.cs
@@ -0,0 +1,15 @@
+﻿namespace RateCheck.Services
+{
+    public interface IProviderService
+    {
+       float GetRate(float amount);
+    }
+
+    public class XoomProviderService : IProviderService
+    {
+        public float GetRate(float amount)
+        {
+            return (float) 0.9;
+        }
+    }
+}
